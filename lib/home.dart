6c114e5d1e9282b8c24b7f1af4ced878472bf0c0
philippaul1998 <pippu98@gmail.com>
2020-02-17 00:41:03 +0530

    Play Feature Implemented

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:audioplayer/audioplayer.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -9,6 +12,21 @@ class Home extends StatefulWidget{
 }
 
 class HomeState extends State<Home>{
+
+
+  List<FileSystemEntity> _files;
+  List<FileSystemEntity> _songs = [];
+  AudioPlayer audioPlayer;
+  AudioPlayerState playerState;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getSongs();
+    audioPlayer = new AudioPlayer();
+    playerState = AudioPlayerState.PAUSED;
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,16 +36,54 @@ class HomeState extends State<Home>{
         centerTitle: true,
       ),
       body: ListView.builder(
-        itemCount: 25,
+        itemCount: _songs.length,
           itemBuilder: (BuildContext buildContext, int index){
-            return Card(
+            return GestureDetector(
+                child:Card(
               child: Container(
                 padding: EdgeInsets.all(16),
-                  child:Text("Music File "+ index.toString()
+                  child:Text(
+                      _songs.elementAt(index).path.split('/').last.replaceAll(".mp3", "")
                   )
               ),
+            ),
+              onTap: (){
+                  if(playerState == AudioPlayerState.PAUSED){
+                    play(_songs[index]);
+                  }else{
+                    pause();
+                  }
+
+            },
             );
           }),
     );
+  }
+
+  Future<void> play(FileSystemEntity file) async {
+    await audioPlayer.play(file.path);
+    setState(() => playerState = AudioPlayerState.PLAYING);
+  }
+
+  Future<void> pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = AudioPlayerState.PAUSED);
+  }
+
+  getSongs(){
+    Directory dir = Directory('/storage/emulated/0/');
+    String mp3Path = dir.toString();
+    print(mp3Path);
+    _files = dir.listSync(recursive: true, followLinks: false);
+    for(FileSystemEntity entity in _files) {
+      String path = entity.path;
+      if(path.endsWith('.mp3'))
+        setState(() {
+          _songs.add(entity);
+        });
+
+    }
+    print(_songs);
+    print(_songs.length);
   }
 }
